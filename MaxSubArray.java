da7b11cb72a5c9c36d6ce4f7c1e5319f0b3c5f72
kanojiyakhushboo02 <kanojiyakhushboo48@gmail.com>
2026-06-20 21:12:33 +0530

    Solved MaxSubarray using kadanes Algo

--- MaxSubArray.java
+++ MaxSubArray.java
@@ -1,18 +1,39 @@
 public class MaxSubArray {
 
-    public static int maxSubArray(int nums[]){
-        int maxSum = Integer.MIN_VALUE;
-        int currentSum = 0;
-        for(int i=0; i<nums.length; i++){
-            for(int j=i+1; j<nums.length; j++){
-                
-                currentSum += nums[j];
-                maxSum = Math.max(maxSum, currentSum);
-            }
-                
-        }
-        return maxSum;
-    }
+//    public static int maxSubArray(int nums[]) {
+
+//     int maxSum = Integer.MIN_VALUE;
+
+//     for(int i = 0; i < nums.length; i++) {
+
+//         int currentSum = 0;
+
+//         for(int j = i; j < nums.length; j++) {
+
+//             currentSum += nums[j];
+
+//             maxSum = Math.max(maxSum, currentSum);
+//         }
+//     }
+
+//     return maxSum;
+// }
+
+
+//Kadanes Algorithm 
+public static int maxSubArray(int nums[]) {
+    int maxSum = Integer.MIN_VALUE;
+    int currentSum = 0;
+    
+   for(int i=0; i<nums.length; i++){
+    currentSum += nums[i];
+    maxSum = Math.max(maxSum, currentSum);
+    if(currentSum < 0){
+        currentSum = 0;
+    }   
+   }
+   return maxSum;
+}
     
     public static void main(String args[]){
         
