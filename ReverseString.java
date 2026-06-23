c1714cacd6d31c4b99597252aca95295171e1777
kanojiyakhushboo02 <kanojiyakhushboo48@gmail.com>
2026-06-23 22:10:49 +0530

    reverse a String

---
+++ ReverseString.java
@@ -0,0 +1,24 @@
+public class ReverseString {
+    
+
+    public static void main(String args[]){
+        String str = "HELLO";
+
+        char[] arr = str.toCharArray();
+            
+        int start = 0;
+        int end = arr.length - 1;
+            
+        while(start < end) {
+        
+            char temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+        
+            start++;
+            end--;
+        }
+        
+        System.out.println(new String(arr));
+    }
+}
