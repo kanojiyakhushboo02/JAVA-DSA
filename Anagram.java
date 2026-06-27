4da03b22fbb2d3040fd461da777a1b461e1931bb
kanojiyakhushboo02 <kanojiyakhushboo48@gmail.com>
2026-06-27 12:28:13 +0530

    updated is Anagram code

--- Anagram.java
+++ Anagram.java
@@ -1,32 +1,43 @@
-import java.util.*;
+// import java.util.*;
 
 
 public class Anagram {
-    
+
+    public static boolean isAnagram(String str1, String str2){
+        str1 =  str1.toLowerCase();
+        str2 =  str2.toLowerCase();
+
+       str1 = str1.replace(" ", "");
+       str2 = str2.replace(" ", "");
+
+       int count[ ] = new int[26];
+       //fill
+       for(int i=0; i<str1.length(); i++){
+            count[str1.charAt(i) - 'a']++;
+       }
+       //empty
+       for(int i=0; i<str2.length(); i++){
+            count[str2.charAt(i) - 'a']--;
+       }
+       //check
+       for(int i=0; i<26; i++){
+            if(count[i] != 0){
+                return false;
+            }
+       }
+       return true;
+    }
 
     public static void main(String args[]){
         String str1 = "listen";
-        String str2 = "silent";
+        String str2 = "silekndncnt";
 
-        if(str1.length() != str2.length()){
-            System.out.println("Not an anagram");
-            return;
-        }
+        boolean result = isAnagram(str1, str2);
 
-        char[] arr1 = str1.toCharArray();
-        char[] arr2 = str2.toCharArray();   
-
-        Arrays.sort(arr1);
-        Arrays.sort(arr2);  
-
-        if(arr1.length != arr2.length){
-            System.out.println("Not an anagram");
-            return;
-        }
-        if(Arrays.equals(arr1, arr2)){
-            System.out.println("Is an anagram");
-        } else {
-            System.out.println("Not an anagram");
+        if(result){
+            System.out.println("Is Anagram");
+        }else{
+            System.out.println("Is Not An Anagram");
         }
     }
 }
