bb4fb6ed26192b9b607a57a5a2a23ccd17101523
kanojiyakhushboo02 <kanojiyakhushboo48@gmail.com>
2026-09-11 19:52:21 +0530

    Selection Sort

---
+++ SortingAlgo/SelectionSort.java
@@ -0,0 +1,32 @@
+package SortingAlgo;
+
+import java.util.*;
+
+public class SelectionSort {
+
+    public static void selection(int arr[], int n) {
+        for (int i = 0; i < n - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void main(String args[]) {
+        int arr[] = {64, 25, 12, 22, 11};
+        int n = arr.length;
+
+        selection(arr, n);
+
+        System.out.println("Sorted array:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
