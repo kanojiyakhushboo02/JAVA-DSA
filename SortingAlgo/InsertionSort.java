e18b1fe4bf246857fe6ce97948820eb4cb1a3c18
kanojiyakhushboo02 <kanojiyakhushboo48@gmail.com>
2026-09-12 19:55:29 +0530

    Insertion Sort

---
+++ SortingAlgo/InsertionSort.java
@@ -0,0 +1,29 @@
+package SortingAlgo;
+
+public class InsertionSort {
+
+    public static void insertionSort(int arr[]){
+        for(int i=1; i<arr.length; i++){
+            int key = arr[i];
+            int j = i-1;
+            while(j>=0 && arr[j] > key){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = key;
+        }
+    }
+
+    public static void printArray(int arr[]){
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]){
+        int arr[] = {5, 3, 4, 1};
+        insertionSort(arr);
+        printArray(arr);
+    }
+}
